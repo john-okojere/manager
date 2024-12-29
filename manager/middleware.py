3de42579cc466f9c4a9dd7d11dd30f9e86ceb41e
john-okojere <johnokojere08@gmail.com>
2024-12-29 17:20:28 +0100

    modified:   manager/middleware.py

--- manager/middleware.py
+++ manager/middleware.py
@@ -2,16 +2,23 @@ from django.shortcuts import redirect
 from django.utils.deprecation import MiddlewareMixin
 from django.contrib.auth import logout
 
+
 class RoleBasedLogoutMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        if not (request.user.is_authenticated and (request.user.role == "Manager" or request.user.is_staff or request.user.level >= 3)):
-            logout(request)
-            return redirect('login')
+        # Apply only if the user is authenticated
+        if request.user.is_authenticated:
+            if not (request.user.role == "Manager" or request.user.is_staff or request.user.level >= 3):
+                logout(request)
+                return redirect('login')
+        return None
+
 
 class Redirect404Middleware(MiddlewareMixin):
     def process_response(self, request, response):
-        # Check if the response status code is 404
-        if response.status_code == 404:
-            # Redirect to the dashboard page
-            return redirect('arcade_dashboard')
+        # Apply only if the user is authenticated
+        if request.user.is_authenticated:
+            # Check if the response status code is 404
+            if response.status_code == 404:
+                # Redirect to the dashboard page
+                return redirect('arcade_dashboard')
         return response
